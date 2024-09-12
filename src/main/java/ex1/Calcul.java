e6dd533116a64dc8488d74cf1d16e8100d812a3f
Nabil_Bedjih <nabil.bedjih@gmail.com>
2024-09-12 16:48:49 +0200

    ex1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,12 @@
+package ex1;
+
+public class Calcul {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
