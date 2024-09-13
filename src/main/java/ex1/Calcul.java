f58da92a05d059a8090efe314e99cef859506464
Nabil_Bedjih <nabil.bedjih@gmail.com>
2024-09-13 13:12:06 +0200

    ex1

--- src/main/java/ex1/Calcul.java
+++ src/main/java/ex1/Calcul.java
@@ -1,12 +1,29 @@
 package ex1;
 
+/**
+ * La classe {@code Calcul} fournit des méthodes pour effectuer des calculs arithmétiques simples.
+ */
 public class Calcul {
 
-    public int add(int a, int b) {
+    /**
+     * Additionne deux nombres entiers.
+     *
+     * @param a le premier nombre entier
+     * @param b le deuxième nombre entier
+     * @return la somme des deux nombres
+     */
+    public int ajouter(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    /**
+     * Soustrait le deuxième nombre entier du premier.
+     *
+     * @param a le premier nombre entier
+     * @param b le deuxième nombre entier
+     * @return la différence entre le premier et le deuxième nombre
+     */
+    public int soustraire(int a, int b) {
         return a - b;
     }
 }
--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -2,16 +2,41 @@ package ex1;
 
 import java.util.Date;
 
+/**
+ * La classe {@code Entreprise} représente une entité commerciale avec ses informations de base.
+ */
 public class Entreprise {
 
-    public static final int CAPITAL_MAX = 3000000;
-    public int siret;
-    public String nom;
-    public String adresse;
-    public Date dateCreation;
+    /**
+     * Le capital maximum qu'une entreprise peut avoir.
+     */
+    public static final int CAPITAL_MAXIMAL = 3000000;
 
+    /**
+     * Le numéro SIRET de l'entreprise.
+     */
+    private int siret;
+
+    /**
+     * Le nom de l'entreprise.
+     */
+    private String nom;
+
+    /**
+     * L'adresse de l'entreprise.
+     */
+    private String adresse;
+
+    /**
+     * La date de création de l'entreprise.
+     */
+    private Date dateDeCreation;
+
+    /**
+     * Affiche le statut de l'entreprise.
+     */
     public void afficherStatut() {
-
     }
+
 
 }
